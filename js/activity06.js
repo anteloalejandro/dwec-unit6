67a9eebb430974466542df376bffdb679dfc124a
Alejandro Antelo <alfasant@protonmail.com>
2022-11-22 15:34:13 +0100

    Improved regex

--- js/activity06.js
+++ js/activity06.js
@@ -1,6 +1,6 @@
 function checkURL(url) {
   // This regex checks URLs with users, passwords, domains & TLDs, ports and routes (probably).
-  const regex = /^[a-z]{1,4}:[\/]{0,3}([0-9a-zA-Z\.-]+(:.*)?@)?([0-9a-zA-z]+\.)[0-9a-zA-Z]{2,3}(:[0-9]{0,5})?(\/[a-zA-Z0-9\.\/-]+)?$/
+  const regex = /^[a-z]{1,5}:[\/]{0,3}([0-9a-zA-Z\.-]+(:.*)?@)?([0-9a-zA-z]+\.)+([0-9a-zA-Z]{2,3}|[0-9]{1,3})(:[0-9]{1,5})?(\/[a-zA-Z0-9\.\/-?&_+]*)?$/
 
   return regex.test(url)
 }
